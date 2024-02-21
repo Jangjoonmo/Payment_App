5ed34ca774092a6e13d47eed68267deca10ffc2a
Jangmo <jjmo97@naver.com>
2024-02-21 18:29:09 +0900

    Build: Project 생성

--- delight_labs_assignment/SceneDelegate.swift
+++ delight_labs_assignment/SceneDelegate.swift
@@ -13,10 +13,22 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        self.window = UIWindow(windowScene: windowScene)
+        
+        let tabBarController = UITabBarController()
+        
+        let mainVC = ViewController()
+        
+        self.window?.rootViewController = tabBarController
+        
+        mainVC.tabBarItem = UITabBarItem(title: "", image: UIImage(named: "TabBarMain"), tag: 0)
+        tabBarController.tabBar.backgroundColor = .black
+        tabBarController.tabBar.tintColor = .white
+        
+        self.window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
